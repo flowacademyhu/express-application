a5abd14cd3ac4b7d1b76cbe2d9947b0e58f68cd2
Agi <nemes.agi96@gmail.com>
2018-08-06 11:06:18 +0200

    wip

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express = require('express');
-const handlebars = require('express-handlebars');
+const exphbs = require('express-handlebars');
 
 const app = express();
 
+app.engine('handlebars', exphbs({defaultLayout: 'main', layoutsDir: './app/views/layouts'}));
+app.set('view engine', 'handlebars');
+app.set('views', './app/views');
+
 app.get('/', (req, res) => {
-  res.json({ok: true});
+  res.render('index');
 });
 
 app.listen(8080);
