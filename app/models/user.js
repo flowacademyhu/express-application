dc344c6688f4b86d12d07c8c27f2883a0ec9a6b8
Ybira <koszotam@gmail.com>
2018-08-09 14:11:06 +0200

    index.js fix

--- app/models/user.js
+++ app/models/user.js
@@ -4,7 +4,7 @@ module.exports = (sequelize, DataTypes) => {
     lastName: DataTypes.STRING,
     email: DataTypes.STRING
   }, {});
-  User.associate = function(models) {
+  User.associate = function (models) {
     // associations can be defined here
   };
   return User;
