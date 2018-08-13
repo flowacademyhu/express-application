49c21c35501eff1f1651d2f07cd15d09ed208b3d
Godfrid <godfrid@gmail.com>
2018-08-13 11:29:20 +0200

    FK places added

--- app/models/cart-line.js
+++ app/models/cart-line.js
@@ -1,6 +1,8 @@
 module.exports = (sequelize, DataTypes) => {
   var CartLine = sequelize.define('CartLine', {
-    quantity: DataTypes.INTEGER
+    quantity: DataTypes.INTEGER,
+    productId: DataTypes.INTEGER,
+    userId: DataTypes.INTEGER
   }, {});
   CartLine.associate = function (models) {
     // associations can be defined here
