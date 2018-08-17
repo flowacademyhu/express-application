1895ec5bd64452f1714e48f143c234ba4c57af1c
Godfrid <godfrid@gmail.com>
2018-08-17 11:42:47 +0200

    migration fix

--- migrations/20180816134659-setting-foreign-key-constraints.js
+++ migrations/20180816134659-setting-foreign-key-constraints.js
@@ -2,107 +2,112 @@
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    queryInterface.addConstraint('Users', ['addressId'], {
+    let promises = [];
+
+    promises.push(queryInterface.addConstraint('Users', ['addressId'], {
       type: 'FOREIGN KEY',
       name: 'fk_Users_Addresses',
       references: {
         table: 'Addresses',
         field: 'id'
       }
-    });
+    }));
 
-    queryInterface.addConstraint('OrderLines', ['orderId'], {
+    promises.push(queryInterface.addConstraint('OrderLines', ['orderId'], {
       type: 'FOREIGN KEY',
       name: 'fk_OrderLines_Orders',
       references: {
         table: 'Orders',
         field: 'id'
       }
-    });
+    }));
 
-    queryInterface.addConstraint('OrderLines', ['productId'], {
+    promises.push(queryInterface.addConstraint('OrderLines', ['productId'], {
       type: 'FOREIGN KEY',
       name: 'fk_OrderLines_Products',
       references: {
         table: 'Products',
         field: 'id'
       }
-    });
+    }));
 
-    queryInterface.addConstraint('CartLines', ['productId'], {
+    promises.push(queryInterface.addConstraint('CartLines', ['productId'], {
       type: 'FOREIGN KEY',
       name: 'fk_CartLines_Products',
       references: {
         table: 'Products',
         field: 'id'
       }
-    });
+    }));
 
-    queryInterface.addConstraint('CartLines', ['userId'], {
+    promises.push(queryInterface.addConstraint('CartLines', ['userId'], {
       type: 'FOREIGN KEY',
       name: 'fk_CartLines_Users',
       references: {
         table: 'Users',
         field: 'id'
       }
-    });
+    }));
 
-    queryInterface.addConstraint('Comments', ['productId'], {
+    promises.push(queryInterface.addConstraint('Comments', ['productId'], {
       type: 'FOREIGN KEY',
       name: 'fk_Comments_Products',
       references: {
         table: 'Products',
         field: 'id'
       }
-    });
+    }));
 
-    queryInterface.addConstraint('Comments', ['userId'], {
+    promises.push(queryInterface.addConstraint('Comments', ['userId'], {
       type: 'FOREIGN KEY',
       name: 'fk_Comments_Users',
       references: {
         table: 'Users',
         field: 'id'
       }
-    });
+    }));
 
-    queryInterface.addConstraint('Orders', ['userId'], {
+    promises.push(queryInterface.addConstraint('Orders', ['userId'], {
       type: 'FOREIGN KEY',
       name: 'fk_Orders_Users',
       references: {
         table: 'Users',
         field: 'id'
       }
-    });
+    }));
 
-    queryInterface.addConstraint('Orders', ['billingAddressId'], {
+    promises.push(queryInterface.addConstraint('Orders', ['billingAddressId'], {
       type: 'FOREIGN KEY',
       name: 'fk_Orders_Addresses_1',
       references: {
         table: 'Addresses',
         field: 'id'
       }
-    });
+    }));
 
-    queryInterface.addConstraint('Orders', ['deliveryAddressId'], {
+    promises.push(queryInterface.addConstraint('Orders', ['deliveryAddressId'], {
       type: 'FOREIGN KEY',
       name: 'fk_Orders_Addresses_2',
       references: {
         table: 'Addresses',
         field: 'id'
       }
-    });
+    }));
+    return Promise.all(promises);
   },
 
   down: (queryInterface, Sequelize) => {
-    queryInterface.removeConstraint('Users', 'fk_Users_Addresses');
-    queryInterface.removeConstraint('OrderLines', 'fk_OrderLines_Orders');
-    queryInterface.removeConstraint('OrderLines', 'fk_OrderLines_Products');
-    queryInterface.removeConstraint('CartLines', 'fk_CartLines_Products');
-    queryInterface.removeConstraint('CartLines', 'fk_CartLines_Users');
-    queryInterface.removeConstraint('Comments', 'fk_Comments_Products');
-    queryInterface.removeConstraint('Comments', 'fk_Comments_Users');
-    queryInterface.removeConstraint('Orders', 'fk_Orders_Users');
-    queryInterface.removeConstraint('Addresses', 'fk_Orders_Addresses_1');
-    queryInterface.removeConstraint('Addresses', 'fk_Orders_Addresses_2');
+    let promises = [];
+    promises.push(queryInterface.removeConstraint('Users', 'fk_Users_Addresses'));
+    promises.push(queryInterface.removeConstraint('OrderLines', 'fk_OrderLines_Orders'));
+    promises.push(queryInterface.removeConstraint('OrderLines', 'fk_OrderLines_Products'));
+    promises.push(queryInterface.removeConstraint('CartLines', 'fk_CartLines_Products'));
+    promises.push(queryInterface.removeConstraint('CartLines', 'fk_CartLines_Users'));
+    promises.push(queryInterface.removeConstraint('Comments', 'fk_Comments_Products'));
+    promises.push(queryInterface.removeConstraint('Comments', 'fk_Comments_Users'));
+    promises.push(queryInterface.removeConstraint('Orders', 'fk_Orders_Users'));
+    promises.push(queryInterface.removeConstraint('Addresses', 'fk_Orders_Addresses_1'));
+    promises.push(queryInterface.removeConstraint('Addresses', 'fk_Orders_Addresses_2'));
+    return Promise.all(promises);
   }
 };
