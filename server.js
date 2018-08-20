1c1d367b7b2f326a7fd2873ad8bee8a429cb39c6
g4bor <gabor.tanacs@yahoo.com>
2018-08-20 22:41:13 +0200

    category listing middleware and category sidebar added

--- server.js
+++ server.js
@@ -2,14 +2,21 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 
+// method override
 const methodOverride = require('method-override');
 app.use(methodOverride('_method'));
 
+// handlebars settings
 const exphbs = require('express-handlebars');
 app.engine('handlebars', exphbs({defaultLayout: 'main', layoutsDir: './app/views/layouts'}));
 app.set('view engine', 'handlebars');
 app.set('views', './app/views');
 
+// category sidebar
+const sidebar = require('./sidebar');
+app.use(sidebar);
+
+// bodyparser settings
 const users = require('./app/controllers/users');
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
