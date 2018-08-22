5a04e3e0afdcac256d86656826dd279b6ea4e07a
Agi <nemes.agi96@gmail.com>
2018-08-22 09:22:38 +0200

    categories new design

--- app/controllers/categories.js
+++ app/controllers/categories.js
@@ -4,8 +4,20 @@ const models = require('../models');
 const Category = models.Category;
 const Product = models.Product;
 
+// Build breadcrumb
+const buildBreadcrumb = (id, elements) => {
+  Category.findById(id).then(categoryRecord => {
+    elements.shift(categoryRecord);
+    if (categoryRecord.parentId) {
+      buildBreadcrumb(categoryRecord.parentId, elements);
+    }
+  });
+};
+
 // Show
 categories.get('/:id', (req, res) => {
+  let breadcrumb = [];
+  buildBreadcrumb(req.params.id, breadcrumb);
   const getCategory = Category.findById(req.params.id);
   const getProducts = Product.findAll({where: {categoryId: req.params.id}});
   Promise.all([getCategory, getProducts]).then(results => {
