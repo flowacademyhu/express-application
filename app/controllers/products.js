385051c44fd85b2827f4a58baaac440be9d27e13
Ybira <koszotam@gmail.com>
2018-08-23 11:47:51 +0200

    ADD add to cart actions in products and categories

--- app/controllers/products.js
+++ app/controllers/products.js
@@ -4,6 +4,7 @@ const models = require('../models');
 const Product = models.Product;
 const Comment = models.Comment;
 const User = models.User;
+const CartLine = models.CartLine;
 
 // Show
 products.get('/:id', (req, res) => {
@@ -13,7 +14,20 @@ products.get('/:id', (req, res) => {
     const product = results[0];
     const comments = results[1];
     let ctx = { product, comments, user: req.user };
+    // res.json(ctx);
     res.render('products/show.handlebars', ctx);
+  });
+});
+
+// ADD items to cart
+products.put('/:id', (req, res) => {
+  // res.json({productId: req.params.id, userId: req.user.id, quantity: req.body.quantity});
+  CartLine.create({
+    quantity: req.body.quantity,
+    productId: req.params.id,
+    userId: req.user.id
+  }).then(() => {
+    res.redirect(`/products/${req.params.id}`);
   });
 });
 
