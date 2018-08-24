1d64510945b475bc3880adec5ade99853326558f
Ybira <koszotam@gmail.com>
2018-08-24 14:37:05 +0200

    HOTFIX search add to cart action, and handlebars fix for add to cart visibility

--- app/controllers/searches.js
+++ app/controllers/searches.js
@@ -2,10 +2,14 @@ const express = require('express');
 const searches = express.Router();
 const models = require('../models');
 const Product = models.Product;
+const CartLine = models.CartLine;
 const Sequelize = require('sequelize');
+
+let tempQuery;
 
 // Index
 searches.get('/', (req, res) => {
+  tempQuery = req.query.search;
   Product.findAll({
     where: {
       [Sequelize.Op.or]: [
@@ -20,8 +24,20 @@ searches.get('/', (req, res) => {
       ]
     }
   }).then((searchedProduct) => {
-    let ctx = { products: searchedProduct };
+    let ctx = { products: searchedProduct, user: req.user };
     res.render('search/index', ctx);
+  });
+});
+
+// ADD items to cart
+searches.put('/:id', (req, res) => {
+  // res.json({categoryId: tempCatId, productId: req.params.id, userId: req.user.id, quantity: req.body.quantity});
+  CartLine.create({
+    quantity: req.body.quantity,
+    productId: req.params.id,
+    userId: req.user.id
+  }).then(() => {
+    res.redirect(`/search/?search=${tempQuery}`);
   });
 });
 
